e617406c105dac5a26797379537bc5be7e41b4bc
bitcamp <auddnekt@naver.com>
2020-03-10 15:26:41 +0900

    DTO내용추가

--- src/main/java/com/bitcamp/TFDTO/ViewListDTO.java
+++ src/main/java/com/bitcamp/TFDTO/ViewListDTO.java
@@ -1,5 +1,12 @@
 package com.bitcamp.TFDTO;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
 public class ViewListDTO {
 	private int StoreNo;
 	private int UserNo;
